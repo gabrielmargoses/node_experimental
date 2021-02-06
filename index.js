3e7fb71fb14a22bf545133d7440a6fb9c94cb5de
Gabriel Margoses <g.margoses@24sevres.com>
2021-02-06 19:12:41 +0100

    testing clustering

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require('express');
-const app = express();
+process.env.UV_THREADPOOL_SIZE = 1;
+const cluster = require('cluster');
 
-app.get('/', (req, res) => {
-    res.send('Hello Gabo');
-});
+if (cluster.isMaster) {
+    cluster.fork();
+} else {
+    const express = require('express');
+    const crypto = require('crypto');
+    const app = express();
 
-app.listen(3000);
+    app.get('/', (req, res) => {
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send('Hello Gabo');
+        });
+    });
+
+    app.get('/flash', (req, res) => {
+        res.send('Hello Flash Gabo');
+    });
+
+    app.listen(3000);
+}
